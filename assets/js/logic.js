f1b3dfe4ac804d51e7a69379b4cab5db2af272f7
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-17 03:42:48 -0700

    Added console logs and fixed html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,23 @@
 var APIkey = "b3ffbfc2d2d5a7ba5ea274ccea458047";
 
-searchFunc()
-function searchFunc() {
+$(document).ready(function() {
+    $("#btnSubmit").click(function(){
+        alert("button");
+    }); 
+});
+
+searchPosFunc()
+function searchPosFunc() {
     var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${seachParam}&limit=1&appid=${APIkey}`
     fetch(searchUrl)
     .then(response => response.json())
-    .then(data => searchFunc(data));
-    
-    
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`
+    .then(data => searchWeatherFunc(data));
+}
+
+function searchWeatherFunc(dataObj) {
+    console.log(dataObj)
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
 }
