9305bba4d01f8f0e201715ec0cb9c74aed44e805
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-17 03:59:24 -0700

    Added Functionality search button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,14 @@
 var APIkey = "b3ffbfc2d2d5a7ba5ea274ccea458047";
-
+let state;
+let city;
 $(document).ready(function() {
-    $("#btnSubmit").click(function(){
-        alert("button");
+    $("#searchButton").click(function(){
+        searchInput = $("#searchInput").val();
+        searchPosFunc(searchInput)
     }); 
 });
 
-searchPosFunc()
-function searchPosFunc() {
+function searchPosFunc(seachParam) {
     var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${seachParam}&limit=1&appid=${APIkey}`
     fetch(searchUrl)
     .then(response => response.json())
@@ -16,6 +17,10 @@ function searchPosFunc() {
 
 function searchWeatherFunc(dataObj) {
     console.log(dataObj)
+    var lat = dataObj[0].lat;
+    var lon = dataObj[0].lon;
+    var city = dataObj[0].name;
+    var state = dataObj[0].state;
     var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
     fetch(url)
     .then(response => response.json())
