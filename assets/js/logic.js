e92c79e5d63cfe9d6d61824b4027cf8d2148fd49
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-17 04:52:41 -0700

    Added 5 day forecast

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,18 @@
 var APIkey = "b3ffbfc2d2d5a7ba5ea274ccea458047";
 let state;
 let city;
-$(document).ready(function() {
-    $("#searchButton").click(function(){
+$(document).ready(function () {
+    $("#searchButton").click(function () {
         searchInput = $("#searchInput").val();
         searchPosFunc(searchInput)
-    }); 
+    });
 });
 
 function searchPosFunc(seachParam) {
     var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${seachParam}&limit=1&appid=${APIkey}`
     fetch(searchUrl)
-    .then(response => response.json())
-    .then(data => searchWeatherFunc(data));
+        .then(response => response.json())
+        .then(data => searchWeatherFunc(data));
 }
 
 function searchWeatherFunc(dataObj) {
@@ -21,17 +21,29 @@ function searchWeatherFunc(dataObj) {
     var lon = dataObj[0].lon;
     city = dataObj[0].name;
     state = dataObj[0].state;
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
     fetch(url)
-    .then(response => response.json())
-    .then(data => WeatherRender(data));
+        .then(response => response.json())
+        .then(data => WeatherRender(data));
 }
 
 function WeatherRender(dataObj) {
     console.log(dataObj);
     $('#place').text(`${state}, ${city}`)
-    $('#todayTemp').text(dataObj.current.temp);
-    $('#todayWind').text(dataObj.current.wind_speed);
-    $('#todayHumid').text(dataObj.current.humidity);
+    $('#todayTemp').text(`${dataObj.current.temp} °F`);
+    $('#todayWind').text(`${dataObj.current.wind_speed} Mph`);
+    $('#todayHumid').text(`${dataObj.current.humidity}%`);
     $('#todayUvi').text(dataObj.current.uvi);
+    fiveDayForecast(dataObj);
+}
+
+function fiveDayForecast(dataObj) {
+    for (let index = 1; index < 6; index++) {
+        console.log(dataObj.daily[index])
+        $(`#day${index}Ahead`).text(moment().add(index, 'days').format('MMMM DD YYYY'));
+        $(`#day${index}Ahead`).append(`<p>Temperature: ${dataObj.daily[index].temp.day} °F</p>`)
+        $(`#day${index}Ahead`).append(`<p>Wind: ${dataObj.daily[index].wind_speed} Mph</p>`)
+        $(`#day${index}Ahead`).append(`<p>Humidity: ${dataObj.daily[index].humidity}%</p>`)
+        $(`#day${index}Ahead`).append(`<p>UV Index: ${dataObj.daily[index].uvi}</p>`)
+    }
 }
